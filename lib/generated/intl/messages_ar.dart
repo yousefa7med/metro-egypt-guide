7ac1d2f037b19c18c1d16e1499a263da0e64d76f
yousefa7med <youssefahmedserag@gmail.com>
2025-08-21 21:00:46 +0300

    ar localization

---
+++ lib/generated/intl/messages_ar.dart
@@ -0,0 +1,40 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a ar locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'ar';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+    "Arabic": MessageLookupByLibrary.simpleMessage("اللغة العربية"),
+    "Dark": MessageLookupByLibrary.simpleMessage("داكن"),
+    "English": MessageLookupByLibrary.simpleMessage("اللغة الانجليزية"),
+    "Home": MessageLookupByLibrary.simpleMessage("الصفحة الرئيسية"),
+    "Light": MessageLookupByLibrary.simpleMessage("فاتح"),
+    "Lines": MessageLookupByLibrary.simpleMessage("خطوط المترو"),
+    "SelectMetroLine": MessageLookupByLibrary.simpleMessage("اختر خط المترو"),
+    "Settings": MessageLookupByLibrary.simpleMessage("الاعدادت"),
+    "Theme": MessageLookupByLibrary.simpleMessage("ثيم"),
+    "lang": MessageLookupByLibrary.simpleMessage("اللغة"),
+    "metroGuide": MessageLookupByLibrary.simpleMessage("مرشد المترو "),
+    "nearestStation": MessageLookupByLibrary.simpleMessage("اقرب محطة"),
+    "preTheme": MessageLookupByLibrary.simpleMessage("اختر الثيم الذي تفضله"),
+    "selectLang": MessageLookupByLibrary.simpleMessage("اختر اللغة"),
+  };
+}
--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -167,7 +167,10 @@ class AppLocalizationDelegate extends LocalizationsDelegate<S> {
   const AppLocalizationDelegate();
 
   List<Locale> get supportedLocales {
-    return const <Locale>[Locale.fromSubtags(languageCode: 'en')];
+    return const <Locale>[
+      Locale.fromSubtags(languageCode: 'en'),
+      Locale.fromSubtags(languageCode: 'ar'),
+    ];
   }
 
   @override
