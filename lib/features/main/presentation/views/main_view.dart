9dc80b6b528b051beff7f4a4e83b522775d33e3f
yousefa7med <youssefahmedserag@gmail.com>
2025-10-05 19:49:26 +0300

    refactor: Update MainView to use StatelessWidget and improve navigation bar configuration; update dependencies for persistent_bottom_nav_bar

--- lib/features/main/presentation/views/main_view.dart
+++ lib/features/main/presentation/views/main_view.dart
@@ -1,91 +1,111 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:metro_egypt_guide/core/Helper/functions/functions.dart';
 import 'package:metro_egypt_guide/core/utilities/app_color.dart';
 import 'package:metro_egypt_guide/features/home/controller/trip_cubit/trip_cubit.dart';
 import 'package:metro_egypt_guide/features/home/presentation/views/home_view.dart';
 import 'package:metro_egypt_guide/features/lines/presentation/views/lines_view.dart';
 import 'package:metro_egypt_guide/features/settings/presentation/views/settings_view.dart';
 import 'package:metro_egypt_guide/generated/l10n.dart';
-import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
+import 'package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart';
 
-class MainView extends StatefulWidget {
+class MainView extends StatelessWidget {
   const MainView({super.key});
-
-  @override
-  State<MainView> createState() => _MainViewState();
-}
-
-class _MainViewState extends State<MainView> {
-  late PersistentTabController _controller;
-
-  @override
-  void initState() {
-    _controller = PersistentTabController(initialIndex: 0);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return PersistentTabView(
-      navBarStyle: NavBarStyle.style6,
-      controller: _controller,
-      context,
-      screens: _screenList(),
-      items: _navBarsItems(context),
+      tabs: _tabs(),
+      navBarBuilder: (navBarConfig) => Style7BottomNavBar(
+        navBarConfig: navBarConfig,
+        height: 58,
+        navBarDecoration: NavBarDecoration(
+          color: isDark(context) ? AppColor.darkSurface : Colors.white,
+        ),
+      ),
+
       handleAndroidBackButtonPress: true,
       resizeToAvoidBottomInset: true,
 
-      hideNavigationBarWhenKeyboardAppears: true,
-      popBehaviorOnSelectedNavBarItemPress: PopBehavior.all,
-      animationSettings: const NavBarAnimationSettings(
-        navBarItemAnimation: ItemAnimationSettings(
-          duration: Duration(milliseconds: 400),
-          curve: Curves.ease,
-        ),
-        screenTransitionAnimation: ScreenTransitionAnimationSettings(
-          animateTabTransition: true,
-          duration: Duration(milliseconds: 200),
-          screenTransitionAnimationType: ScreenTransitionAnimationType.slide,
-        ),
-      ),
-      confineToSafeArea: true,
-      navBarHeight: kBottomNavigationBarHeight,
+      // useRootNavigator: true,
+      // hideNavigationBarWhenKeyboardAppears: true,
+
+      // popBehaviorOnSelectedNavBarItemPress: PopBehavior.all,
+      // animationSettings: const NavBarAnimationSettings(
+      //   navBarItemAnimation: ItemAnimationSettings(
+      //     duration: Duration(milliseconds: 400),
+      //     curve: Curves.ease,
+      //   ),
+      //   screenTransitionAnimation: ScreenTransitionAnimationSettings(
+      //     animateTabTransition: true,
+      //     duration: Duration(milliseconds: 400),
+      //     screenTransitionAnimationType: ScreenTransitionAnimationType.slide,
+      //   ),
+      // ),
+      //   confineToSafeArea: true,
+      //   navBarHeight: kBottomNavigationBarHeight,
     );
   }
 }
 
-List<Widget> _screenList() => [
-  BlocProvider(create: (context) => TripCubit(), child: const HomeView()),
-  const LinesView(),
-  const SettingsView(),
-];
+// List<Widget> _screenList() => [
+//   BlocProvider(create: (context) => TripCubit(), child: const HomeView()),
+//   const LinesView(),
+//   const SettingsView(),
+// ];
 
-List<PersistentBottomNavBarItem> _navBarsItems(BuildContext context) => [
-  PersistentBottomNavBarItem(
-    icon: const Padding(
-      padding: EdgeInsets.only(top: 8.0),
-      child: Icon(Icons.home),
+// List<PersistentBottomNavBarItem> _navBarsItems(BuildContext context) => [
+//   PersistentBottomNavBarItem(
+//     icon: const Padding(
+//       padding: EdgeInsets.only(top: 8.0),
+//       child: Icon(Icons.home),
+//     ),
+//     title: (S.of(context).Home),
+//     activeColorPrimary: AppColor.primaryColor,
+//     inactiveColorPrimary: Colors.grey,
+//   ),
+//   PersistentBottomNavBarItem(
+//     icon: const Icon(Icons.route),
+//     title: (S.of(context).Lines),
+//     activeColorPrimary: AppColor.primaryColor,
+//     inactiveColorPrimary: Colors.grey,
+//   ),
+//   PersistentBottomNavBarItem(
+//     icon: const Icon(Icons.settings),
+//     title: (S.of(context).Settings),
+//     activeColorPrimary: AppColor.primaryColor,
+//     inactiveColorPrimary: Colors.grey,
+//   ),
+// ];
+
+List<PersistentTabConfig> _tabs() => [
+  PersistentTabConfig(
+    screen: BlocProvider(
+      create: (context) => TripCubit(),
+      child: const HomeView(),
     ),
-    title: (S.of(context).Home),
-    activeColorPrimary: AppColor.primaryColor,
-    inactiveColorPrimary: Colors.grey,
+    item: ItemConfig(
+      icon: const Icon(Icons.home),
+      title: S.current.Home,
+      activeForegroundColor: AppColor.primaryColor,
+      inactiveForegroundColor: Colors.white70,
+    ),
   ),
-  PersistentBottomNavBarItem(
-    icon: const Icon(Icons.route),
-    title: (S.of(context).Lines),
-    activeColorPrimary: AppColor.primaryColor,
-    inactiveColorPrimary: Colors.grey,
+  PersistentTabConfig(
+    screen: const LinesView(),
+    item: ItemConfig(
+      icon: const Icon(Icons.route),
+      title: S.current.Lines,
+      activeForegroundColor: AppColor.primaryColor,
+      inactiveForegroundColor: Colors.white70,
+    ),
   ),
-  PersistentBottomNavBarItem(
-    icon: const Icon(Icons.settings),
-    title: (S.of(context).Settings),
-    activeColorPrimary: AppColor.primaryColor,
-    inactiveColorPrimary: Colors.grey,
+  PersistentTabConfig(
+    screen: const SettingsView(),
+    item: ItemConfig(
+      icon: const Icon(Icons.settings),
+      title: S.current.Settings,
+      activeForegroundColor: AppColor.primaryColor,
+      inactiveForegroundColor: Colors.white70,
+    ),
   ),
 ];
