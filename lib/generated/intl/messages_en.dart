dab27804f262687068d9c625c512f27a9ddad4bc
yousefa7med <youssefahmedserag@gmail.com>
2025-10-10 20:15:31 +0300

    first step to translate stations

--- lib/generated/intl/messages_en.dart
+++ lib/generated/intl/messages_en.dart
@@ -22,13 +22,37 @@ class MessageLookup extends MessageLookupByLibrary {
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+    "Abdou_Pasha": MessageLookupByLibrary.simpleMessage("Abdou Pasha"),
+    "Adly_Mansour": MessageLookupByLibrary.simpleMessage("Adly Mansour"),
+    "Ain_Helwan": MessageLookupByLibrary.simpleMessage("Ain Helwan"),
+    "Ain_Shams": MessageLookupByLibrary.simpleMessage("Ain Shams"),
+    "Alf_Masken": MessageLookupByLibrary.simpleMessage("Alf Masken"),
     "AllLines": MessageLookupByLibrary.simpleMessage("All Lines"),
     "Arabic": MessageLookupByLibrary.simpleMessage("Arabic"),
+    "Attaba": MessageLookupByLibrary.simpleMessage("Attaba"),
+    "Cairo_University": MessageLookupByLibrary.simpleMessage(
+      "Cairo University",
+    ),
     "Dark": MessageLookupByLibrary.simpleMessage("Dark"),
+    "Dokki": MessageLookupByLibrary.simpleMessage("Dokki"),
     "EGY": MessageLookupByLibrary.simpleMessage("EGY"),
     "English": MessageLookupByLibrary.simpleMessage("English"),
+    "Fair_Zone": MessageLookupByLibrary.simpleMessage("Fair Zone"),
+    "Faysal": MessageLookupByLibrary.simpleMessage("Faysal"),
     "FindRoute": MessageLookupByLibrary.simpleMessage("Find Route"),
+    "Ghamra": MessageLookupByLibrary.simpleMessage("Ghamra"),
+    "Giza": MessageLookupByLibrary.simpleMessage("Giza"),
+    "Hadayek_Helwan": MessageLookupByLibrary.simpleMessage("Hadayek Helwan"),
+    "Haroun": MessageLookupByLibrary.simpleMessage("Haroun"),
+    "Heliopolis": MessageLookupByLibrary.simpleMessage("Heliopolis"),
+    "Helwan": MessageLookupByLibrary.simpleMessage("Helwan"),
+    "Helwan_University": MessageLookupByLibrary.simpleMessage(
+      "Helwan University",
+    ),
+    "Hesham_Barakat": MessageLookupByLibrary.simpleMessage("Hesham Barakat"),
     "Home": MessageLookupByLibrary.simpleMessage("Home"),
+    "Imbaba": MessageLookupByLibrary.simpleMessage("Imbaba"),
+    "Kozzika": MessageLookupByLibrary.simpleMessage("Kozzika"),
     "Light": MessageLookupByLibrary.simpleMessage("Light"),
     "Line1": MessageLookupByLibrary.simpleMessage("Line 1"),
     "Line2": MessageLookupByLibrary.simpleMessage("Line 2"),
@@ -41,15 +65,35 @@ class MessageLookup extends MessageLookupByLibrary {
     "LocationPermissionRequired": MessageLookupByLibrary.simpleMessage(
       "Location permission required",
     ),
+    "Mar_Girgis": MessageLookupByLibrary.simpleMessage("Mar Girgis"),
+    "Maspero": MessageLookupByLibrary.simpleMessage("Maspero"),
+    "Massara": MessageLookupByLibrary.simpleMessage("Massara"),
+    "Nageeb": MessageLookupByLibrary.simpleMessage("Nageeb"),
+    "Nasser": MessageLookupByLibrary.simpleMessage("Nasser"),
+    "Nozha": MessageLookupByLibrary.simpleMessage("Nozha"),
+    "Opera": MessageLookupByLibrary.simpleMessage("Opera"),
+    "Orabi": MessageLookupByLibrary.simpleMessage("Orabi"),
     "PleaseOpenLocation": MessageLookupByLibrary.simpleMessage(
       "Please Open Location",
     ),
+    "Qebaa": MessageLookupByLibrary.simpleMessage("Qebaa"),
+    "Ring_Road": MessageLookupByLibrary.simpleMessage("Ring Road"),
+    "Rod_El_Farag_Corr": MessageLookupByLibrary.simpleMessage(
+      "Rod El Farag Corr",
+    ),
+    "Saad_Zaghloul": MessageLookupByLibrary.simpleMessage("Saad Zaghloul"),
+    "Safaa_Hegazy": MessageLookupByLibrary.simpleMessage("Safaa Hegazy"),
+    "Saint_Theresa": MessageLookupByLibrary.simpleMessage("Saint Theresa"),
     "SelectMetroLine": MessageLookupByLibrary.simpleMessage(
       "Select Metro Line",
     ),
     "Settings": MessageLookupByLibrary.simpleMessage("Settings"),
+    "Stadium": MessageLookupByLibrary.simpleMessage("Stadium"),
+    "Sudan": MessageLookupByLibrary.simpleMessage("Sudan"),
+    "Tawfikia": MessageLookupByLibrary.simpleMessage("Tawfikia"),
     "Theme": MessageLookupByLibrary.simpleMessage("Theme"),
     "Transfer": MessageLookupByLibrary.simpleMessage("Transfer"),
+    "Wadi_Hof": MessageLookupByLibrary.simpleMessage("Wadi Hof"),
     "addToFav": MessageLookupByLibrary.simpleMessage("Add To Favourite"),
     "backToHome": MessageLookupByLibrary.simpleMessage("Back To Home"),
     "checkDetails": MessageLookupByLibrary.simpleMessage(
@@ -69,6 +113,18 @@ class MessageLookup extends MessageLookupByLibrary {
     "selectLang": MessageLookupByLibrary.simpleMessage("Select your language"),
     "startStation": MessageLookupByLibrary.simpleMessage("Start Station"),
     "to": MessageLookupByLibrary.simpleMessage("To"),
+    "transferBetween12": MessageLookupByLibrary.simpleMessage(
+      "is Transfer station between Line 1 and Line 2",
+    ),
+    "transferBetween13": MessageLookupByLibrary.simpleMessage(
+      "is Transfer station between Line 1 and Line 3",
+    ),
+    "transferBetween23": MessageLookupByLibrary.simpleMessage(
+      "is Transfer station between Line 2 and Line 3",
+    ),
+    "transferBetween33": MessageLookupByLibrary.simpleMessage(
+      "is Transfer station between imbaba branch and cairo Uni branch",
+    ),
     "tripInfo": MessageLookupByLibrary.simpleMessage("Trip Information"),
   };
 }
--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -305,6 +305,373 @@ class S {
   String get to {
     return Intl.message('To', name: 'to', desc: '', args: []);
   }
+
+  /// `is Transfer station between Line 1 and Line 2`
+  String get transferBetween12 {
+    return Intl.message(
+      'is Transfer station between Line 1 and Line 2',
+      name: 'transferBetween12',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `is Transfer station between Line 1 and Line 3`
+  String get transferBetween13 {
+    return Intl.message(
+      'is Transfer station between Line 1 and Line 3',
+      name: 'transferBetween13',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `is Transfer station between Line 2 and Line 3`
+  String get transferBetween23 {
+    return Intl.message(
+      'is Transfer station between Line 2 and Line 3',
+      name: 'transferBetween23',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `is Transfer station between imbaba branch and cairo Uni branch`
+  String get transferBetween33 {
+    return Intl.message(
+      'is Transfer station between imbaba branch and cairo Uni branch',
+      name: 'transferBetween33',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Helwan`
+  String get Helwan {
+    return Intl.message('Helwan', name: 'Helwan', desc: '', args: []);
+  }
+
+  /// `Helwan University`
+  String get Helwan_University {
+    return Intl.message(
+      'Helwan University',
+      name: 'Helwan_University',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Ain Helwan`
+  String get Ain_Helwan {
+    return Intl.message('Ain Helwan', name: 'Ain_Helwan', desc: '', args: []);
+  }
+
+  /// `Wadi Hof`
+  String get Wadi_Hof {
+    return Intl.message('Wadi Hof', name: 'Wadi_Hof', desc: '', args: []);
+  }
+
+  /// `Hadayek Helwan`
+  String get Hadayek_Helwan {
+    return Intl.message(
+      'Hadayek Helwan',
+      name: 'Hadayek_Helwan',
+      desc: '',
+      args: [],
+    );
+  }
+
+  // skipped getter for the 'El-Maasara' key
+
+  // skipped getter for the 'Tora_El-Asmant' key
+
+  /// `Kozzika`
+  String get Kozzika {
+    return Intl.message('Kozzika', name: 'Kozzika', desc: '', args: []);
+  }
+
+  // skipped getter for the 'Tora_El-Balad' key
+
+  // skipped getter for the 'Sakanat_El-Maadi' key
+
+  // skipped getter for the 'El-Maadi' key
+
+  // skipped getter for the 'Hadayek_El-Maadi' key
+
+  // skipped getter for the 'Dar_El-Salam' key
+
+  // skipped getter for the 'El-Zahraa' key
+
+  /// `Mar Girgis`
+  String get Mar_Girgis {
+    return Intl.message('Mar Girgis', name: 'Mar_Girgis', desc: '', args: []);
+  }
+
+  // skipped getter for the 'El-Malek_El-Saleh' key
+
+  // skipped getter for the 'El-Sayeda_Zeinab' key
+
+  /// `Saad Zaghloul`
+  String get Saad_Zaghloul {
+    return Intl.message(
+      'Saad Zaghloul',
+      name: 'Saad_Zaghloul',
+      desc: '',
+      args: [],
+    );
+  }
+
+  // skipped getter for the 'El-Sadat' key
+
+  /// `Orabi`
+  String get Orabi {
+    return Intl.message('Orabi', name: 'Orabi', desc: '', args: []);
+  }
+
+  // skipped getter for the 'El-Shohadaa' key
+
+  /// `Ghamra`
+  String get Ghamra {
+    return Intl.message('Ghamra', name: 'Ghamra', desc: '', args: []);
+  }
+
+  // skipped getter for the 'El-Demerdash' key
+
+  // skipped getter for the 'Manshyet_El-Sadr' key
+
+  // skipped getter for the 'Kobry_El-Qubba' key
+
+  // skipped getter for the 'Hammamet_El-Qubba' key
+
+  // skipped getter for the 'Saray_El-Qubba' key
+
+  // skipped getter for the 'Hadayek_El-Zaytoun' key
+
+  // skipped getter for the 'Helmayet_El-Zaytoun' key
+
+  // skipped getter for the 'El-Mattareya' key
+
+  /// `Ain Shams`
+  String get Ain_Shams {
+    return Intl.message('Ain Shams', name: 'Ain_Shams', desc: '', args: []);
+  }
+
+  // skipped getter for the 'Ezbet_El-Nakhl' key
+
+  // skipped getter for the 'Al-Marg' key
+
+  // skipped getter for the 'New_El-Marg' key
+
+  // skipped getter for the 'Shubra_El-Kheima' key
+
+  // skipped getter for the 'Koleyet_El-Zeraa' key
+
+  // skipped getter for the 'El-Mezallat' key
+
+  // skipped getter for the 'El-Khalafawy' key
+
+  /// `Saint Theresa`
+  String get Saint_Theresa {
+    return Intl.message(
+      'Saint Theresa',
+      name: 'Saint_Theresa',
+      desc: '',
+      args: [],
+    );
+  }
+
+  // skipped getter for the 'Rod_El-Farag' key
+
+  /// `Massara`
+  String get Massara {
+    return Intl.message('Massara', name: 'Massara', desc: '', args: []);
+  }
+
+  /// `Nageeb`
+  String get Nageeb {
+    return Intl.message('Nageeb', name: 'Nageeb', desc: '', args: []);
+  }
+
+  /// `Opera`
+  String get Opera {
+    return Intl.message('Opera', name: 'Opera', desc: '', args: []);
+  }
+
+  /// `Dokki`
+  String get Dokki {
+    return Intl.message('Dokki', name: 'Dokki', desc: '', args: []);
+  }
+
+  // skipped getter for the 'El-Behoos' key
+
+  /// `Cairo University`
+  String get Cairo_University {
+    return Intl.message(
+      'Cairo University',
+      name: 'Cairo_University',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Faysal`
+  String get Faysal {
+    return Intl.message('Faysal', name: 'Faysal', desc: '', args: []);
+  }
+
+  /// `Giza`
+  String get Giza {
+    return Intl.message('Giza', name: 'Giza', desc: '', args: []);
+  }
+
+  // skipped getter for the 'Omm_El-Masryeen' key
+
+  // skipped getter for the 'Sakyet-Mekky' key
+
+  // skipped getter for the 'El-Moneeb' key
+
+  /// `Adly Mansour`
+  String get Adly_Mansour {
+    return Intl.message(
+      'Adly Mansour',
+      name: 'Adly_Mansour',
+      desc: '',
+      args: [],
+    );
+  }
+
+  // skipped getter for the 'El-Haikstep' key
+
+  // skipped getter for the 'Omar_Ibn El-Khattab' key
+
+  /// `Qebaa`
+  String get Qebaa {
+    return Intl.message('Qebaa', name: 'Qebaa', desc: '', args: []);
+  }
+
+  /// `Hesham Barakat`
+  String get Hesham_Barakat {
+    return Intl.message(
+      'Hesham Barakat',
+      name: 'Hesham_Barakat',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Nozha`
+  String get Nozha {
+    return Intl.message('Nozha', name: 'Nozha', desc: '', args: []);
+  }
+
+  // skipped getter for the 'El-Shams_Club' key
+
+  /// `Alf Masken`
+  String get Alf_Masken {
+    return Intl.message('Alf Masken', name: 'Alf_Masken', desc: '', args: []);
+  }
+
+  /// `Heliopolis`
+  String get Heliopolis {
+    return Intl.message('Heliopolis', name: 'Heliopolis', desc: '', args: []);
+  }
+
+  /// `Haroun`
+  String get Haroun {
+    return Intl.message('Haroun', name: 'Haroun', desc: '', args: []);
+  }
+
+  // skipped getter for the 'El-Ahram' key
+
+  // skipped getter for the 'Kolleyet_El-Banat' key
+
+  /// `Stadium`
+  String get Stadium {
+    return Intl.message('Stadium', name: 'Stadium', desc: '', args: []);
+  }
+
+  /// `Fair Zone`
+  String get Fair_Zone {
+    return Intl.message('Fair Zone', name: 'Fair_Zone', desc: '', args: []);
+  }
+
+  // skipped getter for the 'El-Abassiya' key
+
+  /// `Abdou Pasha`
+  String get Abdou_Pasha {
+    return Intl.message('Abdou Pasha', name: 'Abdou_Pasha', desc: '', args: []);
+  }
+
+  // skipped getter for the 'El-Geish' key
+
+  // skipped getter for the 'Bab_El-Shaariya' key
+
+  /// `Attaba`
+  String get Attaba {
+    return Intl.message('Attaba', name: 'Attaba', desc: '', args: []);
+  }
+
+  /// `Nasser`
+  String get Nasser {
+    return Intl.message('Nasser', name: 'Nasser', desc: '', args: []);
+  }
+
+  /// `Maspero`
+  String get Maspero {
+    return Intl.message('Maspero', name: 'Maspero', desc: '', args: []);
+  }
+
+  /// `Safaa Hegazy`
+  String get Safaa_Hegazy {
+    return Intl.message(
+      'Safaa Hegazy',
+      name: 'Safaa_Hegazy',
+      desc: '',
+      args: [],
+    );
+  }
+
+  // skipped getter for the 'Kit-Kat' key
+
+  /// `Sudan`
+  String get Sudan {
+    return Intl.message('Sudan', name: 'Sudan', desc: '', args: []);
+  }
+
+  /// `Imbaba`
+  String get Imbaba {
+    return Intl.message('Imbaba', name: 'Imbaba', desc: '', args: []);
+  }
+
+  // skipped getter for the 'El-Bohy' key
+
+  // skipped getter for the 'El-Qawmia' key
+
+  /// `Ring Road`
+  String get Ring_Road {
+    return Intl.message('Ring Road', name: 'Ring_Road', desc: '', args: []);
+  }
+
+  /// `Rod El Farag Corr`
+  String get Rod_El_Farag_Corr {
+    return Intl.message(
+      'Rod El Farag Corr',
+      name: 'Rod_El_Farag_Corr',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Tawfikia`
+  String get Tawfikia {
+    return Intl.message('Tawfikia', name: 'Tawfikia', desc: '', args: []);
+  }
+
+  // skipped getter for the 'Wadi_El-Nile' key
+
+  // skipped getter for the 'Gamet_El-Dowel' key
+
+  // skipped getter for the 'Boulak_El-Dakrour' key
 }
 
 class AppLocalizationDelegate extends LocalizationsDelegate<S> {
